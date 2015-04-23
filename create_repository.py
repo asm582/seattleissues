bb89354fc3f58dd0c48b1ed8f28c9ddd10601104
asm582 <asm582@users.noreply.github.com>
2015-04-23 11:07:15 -0400

    Update create_repository.py

--- create_repository.py
+++ create_repository.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from tuf.repository_tool import *
 
 import datetime
@@ -9,8 +7,8 @@ repository = create_new_repository("repository")
 repository.root.threshold = 2
 
 # The root keys loaded to satisfy root's threshold.
-donald_root_key = import_ed25519_privatekey_from_file("keystore/user1_root", password='')
-richard_root_key = import_ed25519_privatekey_from_file("keystore/user2_root", password='')
+user1_root_key = import_ed25519_privatekey_from_file("keystore/user1_root", password='passwd')
+user2_root_key = import_ed25519_privatekey_from_file("keystore/user2_root", password='passwd')
 
 # Any of these four root keys may be used to satisfy root's threshold.
 repository.root.add_verification_key(import_ed25519_publickey_from_file("keystore/user1_root.pub"))
@@ -18,22 +16,35 @@ repository.root.add_verification_key(import_ed25519_publickey_from_file("keystor
 repository.root.add_verification_key(import_ed25519_publickey_from_file("keystore/user3_root.pub"))
 repository.root.add_verification_key(import_ed25519_publickey_from_file("keystore/user4_root.pub"))
 
-repository.root.load_signing_key(donald_root_key)
-repository.root.load_signing_key(richard_root_key)
+repository.root.load_signing_key(user1_root_key)
+repository.root.load_signing_key(user2_root_key)
 
 # Add the public keys for the remaining top-level roles.
 repository.targets.add_verification_key(import_ed25519_publickey_from_file("keystore/targets.pub"))
 repository.snapshot.add_verification_key(import_ed25519_publickey_from_file("keystore/snapshot.pub"))
 repository.timestamp.add_verification_key(import_ed25519_publickey_from_file("keystore/timestamp.pub"))
 
-repository.targets.load_signing_key(import_ed25519_privatekey_from_file("keystore/targets", password=''))
-repository.snapshot.load_signing_key(import_ed25519_privatekey_from_file("keystore/snapshot", password=''))
-repository.timestamp.load_signing_key(import_ed25519_privatekey_from_file("keystore/timestamp", password=''))
+repository.targets.load_signing_key(import_ed25519_privatekey_from_file("keystore/targets", password='passwd'))
+repository.snapshot.load_signing_key(import_ed25519_privatekey_from_file("keystore/snapshot", password='passwd'))
+repository.timestamp.load_signing_key(import_ed25519_privatekey_from_file("keystore/timestamp", password='passwd'))
 
 # Create the delegated roles for the PEP 458 security model.  That is, the
 # 'pypi-signed' role and its delegated bins.  Target files are evenly
 # distributed to these bins.
-pypi_signed_pub = import_ed25519_publickey_from_file("keystore/pypi-signed.pub")
-pypi_signed_key = import_ed25519_privatekey_from_file("keystore/pypi-signed", password='pw')
-repository.targets.delegate("pypi-signed", [pypi_signed_pub], [], restricted_paths=["repository/targets/packages/"])
-repository.targets("pypi-signed").load_signing_key(pypi_signed_key)
+#pypi_signed_pub = import_ed25519_publickey_from_file("keystore/pypi-signed.pub")
+#pypi_signed_key = import_ed25519_privatekey_from_file("keystore/pypi-signed", password='passwd')
+#repository.targets.delegate("pypi-signed", [pypi_signed_pub], [], restricted_paths=["repository/targets/packages/"])
+#repository.targets("pypi-signed").load_signing_key(pypi_signed_key)
+
+# Create the bins for the 'pypi-signed' role.
+targets = repository.get_filepaths_in_directory('/home/cib/custominstallerbuilder/repository/targets/base/', recursive_walk=True)
+#repository.targets('pypi-signed').delegate_hashed_bins(targets, [pypi_signed_pub], 16)
+repository.targets.add_targets(targets)
+
+# Set an expiration far into the future so the timestamp role so we can
+# use this repository without having to worry about timestamp expiring.
+repository.timestamp.expiration = datetime.datetime(2044, 10, 28, 12, 8)
+
+# Write all the metadata to disk, signing metadata according to the private
+# keys loaded for each role.
+repository.write()
